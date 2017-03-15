90ff7678e59152948b1038b8641bd0073ba7070a
SwampyCharly <carloentre@gmail.com>
2017-03-15 13:18:23 -0700

    Add files via upload

--- tests/EventsGenerator/src/Generator.java
+++ tests/EventsGenerator/src/Generator.java
@@ -5,13 +5,14 @@ public class Generator
 {	
 	static final String pathFileUsers = "/home/cloudera/tmptest/userid-profile1.tsv";
 	static final String pathFileEvents = "/home/cloudera/tmptest/userid-timestamp-artid-artname-traid-traname.tsv";
-	static final int usersNumber = 10;
-	static final int artistsNumber = 40;
+	static final int usersNumber = 40;
+	static final int artistsNumber = 20;
 	static final int artistForced = 0;
 	static final int userForced = 0;
+	static final int firstNewUserId = 0;
 	
-	static final int newUsers = 200;
-	static final int newEvents = 500;
+	static final int newUsers = 40;
+	static final int newEvents = 1000;
 	
 	public static void main(String[] args)
 	{
@@ -27,7 +28,7 @@ public class Generator
 		
 		for (int i = 0; i < newUsers; i++)
 		{
-			User u = new User(usersNumber + i, usersNumber, artistsNumber);
+			User u = new User(firstNewUserId + i, usersNumber, artistsNumber);
 			writerUsers.println(u.getRow());			
 		}
 		
